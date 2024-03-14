25e51ae450a7ad3155d9b127e72cd377502b0098
Jan <jan.vijfhuizen@outlook.com>
2024-03-14 16:26:02 +0100

    [Add] GNUPlot

--- DeepLearning/Include/pch.h
+++ DeepLearning/Include/pch.h
@@ -3,3 +3,4 @@
 #include <exception>
 #include <new.h>
 #include <iostream>
+#include "gnuplot-iostream.h"
--- DeepLearning/Src/DeepLearning.cpp
+++ DeepLearning/Src/DeepLearning.cpp
@@ -1,7 +1,29 @@
 #include "pch.h"
 
+#include <numeric>
+#include <random>
+
 int main()
 {
-	
+	Gnuplot gp("\"C:\\Program Files\\gnuplot\\bin\\gnuplot.exe\"");
+
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::normal_distribution normDist(0., 1.);
+
+	std::vector<double> v0, v1;
+	for (int i = 0; i < 1000; ++i)
+	{
+		v0.push_back(normDist(mt));
+		v1.push_back(normDist(mt));
+	}
+	std::partial_sum(v0.begin(), v0.end(), v0.begin());
+	std::partial_sum(v1.begin(), v1.end(), v1.begin());
+
+	gp << "set title 'set graph of random lines'\n";
+	gp << "plot '-' with lines title 'v0'," << "'-' with lines title 'v1'\n";
+	gp.send(v0);
+	gp.send(v1);
+	std::cin.get();
 	return 0;
 }

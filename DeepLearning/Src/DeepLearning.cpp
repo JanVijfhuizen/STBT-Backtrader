391f29c88eb444d93735cb2ebae0930b5303110c
Jan <jan.vijfhuizen@outlook.com>
2024-08-17 16:47:31 +0200

    [WIP] Dynamic strategy value assignment.

--- DeepLearning/Src/DeepLearning.cpp
+++ DeepLearning/Src/DeepLearning.cpp
@@ -5,50 +5,49 @@
 #include "JLib/Math.h"
 #include "JLib/Queue.h"
 
-void StockAlgorithm(jv::Arena& tempArena, const jv::bt::World& world, const jv::bt::Portfolio& portfolio,
-	jv::Vector<jv::bt::Call>& calls, const uint32_t offset, void* userPtr)
+[[nodiscard]] float GetMAValue(const jv::bt::TimeSeries& stock, const uint32_t offset, void* userPtr)
 {
-	jv::bt::Call call{};
+	const float maShort = jv::bt::GetMA(stock.close, offset, 10);
+	const float maLong = jv::bt::GetMA(stock.close, offset, 100);
 
-	const auto& stock = world.timeSeries[0];
+	return maShort / maLong - 1.f;
+}
 
-	if(stock.open[offset] > stock.close[offset])
-	{
-		if (portfolio.liquidity - 100 > stock.close[offset])
-		{
-			call.amount = (portfolio.liquidity - 100) / stock.close[offset];
-			call.type = jv::bt::CallType::Buy;
-			call.symbolId = 0;
-			calls.Add() = call;
-		}
-	}
-	else
-	{
-		if (portfolio.liquidity > 10 && portfolio.stocks[0] > 0)
-		{
-			call.amount = portfolio.stocks[0];
-			call.type = jv::bt::CallType::Sell;
-			call.symbolId = 0;
-			calls.Add() = call;
-		}
-	}
-	return;
-
-	const float ma10 = jv::bt::GetMA(stock.close, offset, 10);
-	const float ma100 = jv::bt::GetMA(stock.close, offset, 100);
-
+[[nodiscard]] float GetMomentumValue(const jv::bt::TimeSeries& stock, const uint32_t offset, void* userPtr)
+{
 	float momentum = 0;
 	for (uint32_t i = 0; i < 3; ++i)
 	{
 		const auto ceiling = jv::Max<float>(stock.close[offset + i], stock.open[offset + i]);
 		const auto floor = jv::Min<float>(stock.close[offset + i], stock.open[offset + i]);
 
-		momentum += stock.high[offset + i] - ceiling;
-		momentum -= floor - stock.low[offset + i];
-		//momentum += stock.close[offset + i] - stock.open[offset + i];
+		float f = stock.high[offset + i] - ceiling;
+		f -= floor - stock.low[offset + i];
+		momentum /= abs(stock.close[offset + i] - stock.open[offset + i]);
+		momentum += f;
 	}
+	return momentum / 3;
+}
 
-	if(momentum > 0)
+[[nodiscard]] float GetTrendValue(const jv::bt::TimeSeries& stock, const uint32_t offset, void* userPtr)
+{
+	float trend = 0;
+	for (uint32_t i = 0; i < 3; ++i)
+		trend += stock.close[offset + i] - stock.open[offset + i];
+	return trend / 3;
+}
+
+void StockAlgorithm(jv::Arena& tempArena, const jv::bt::World& world, const jv::bt::Portfolio& portfolio,
+	jv::Vector<jv::bt::Call>& calls, const uint32_t offset, void* userPtr)
+{
+	jv::bt::Call call{};
+
+	const auto& stock = world.timeSeries[0];
+	const float ma = GetMAValue(stock, offset, userPtr);
+	const float momentum = GetMomentumValue(stock, offset, userPtr);
+	const float trend = GetTrendValue(stock, offset, userPtr);
+
+	if(ma + momentum + trend > 0)
 	{
 		if (portfolio.liquidity - 100 > stock.close[offset])
 		{
@@ -59,31 +58,6 @@ void StockAlgorithm(jv::Arena& tempArena, const jv::bt::World& world, const jv::
 		}
 	}
 	else
-	{
-		if (portfolio.liquidity > 10 && portfolio.stocks[0] > 0)
-		{
-			call.amount = portfolio.stocks[0];
-			call.type = jv::bt::CallType::Sell;
-			call.symbolId = 0;
-			calls.Add() = call;
-		}
-	}
-
-	return;
-
-	// buy
-	if(ma10 < ma100 * .98)
-	{
-		if (portfolio.liquidity - 100 > stock.close[offset])
-		{
-			call.amount = (portfolio.liquidity - 100) / stock.close[offset];
-			call.type = jv::bt::CallType::Buy;
-			call.symbolId = 0;
-			calls.Add() = call;
-		}
-	}
-	// sell
-	if (ma10 > ma100 * 1.02)
 	{
 		if (portfolio.liquidity > 10 && portfolio.stocks[0] > 0)
 		{
@@ -107,12 +81,12 @@ int main()
 		symbols[1] = "AMZN";
 		symbols[2] = "TSLA";
 		symbols[3] = "EA";
-		bte = jv::bt::CreateBTE(symbols, 4, 1e-3f);
+		bte = jv::bt::CreateBTE(symbols, 4, 1e-3);
 	}
 
 	jv::bt::TimeSeries timeSeries = bte.backTrader.world.timeSeries[0];
 	//jv::bt::Tracker::Debug(timeSeries.close, 30, true);
-	jv::bt::Tracker::DebugCandles(timeSeries, 0, 400);
+	//jv::bt::Tracker::DebugCandles(timeSeries, 0, 400);
 	
 	jv::bt::TestInfo testInfo{};
 	testInfo.bot = StockAlgorithm;

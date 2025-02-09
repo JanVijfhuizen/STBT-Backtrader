10cec28c5f320f0112ca6b2d4a6b6ff4fe034a3e
Jannie <jan.vijfhuizen@outlook.com>
2025-02-09 20:37:50 +0100

    [Add] Scripting.

--- DeepLearning/Src/DeepLearning.cpp
+++ DeepLearning/Src/DeepLearning.cpp
@@ -6,9 +6,26 @@
 #include <Renderer.h>
 #include <STBT.h>
 
+extern "C"
+{
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
 int main()
 {
-	srand(time(nullptr));
+	lua_State* L = luaL_newstate();
+	luaL_openlibs(L);
+	luaL_dofile(L, "Scripts/hello.lua");
+
+	lua_getglobal(L, "fib");
+	lua_pushnumber(L, 13);
+	lua_call(L, 1, 1);
+	int result = (int)lua_tointeger(L, -1);
+	lua_pop(L, 1);
+
+	lua_close(L);
 
 	auto stui = jv::ai::CreateSTBT();
 

0df13adc8a8b1cf41a67da1fd0086999db5332ce
Jannie <jan.vijfhuizen@outlook.com>
2025-02-10 15:39:57 +0100

    [WIP] Adding lua access functions.

--- DeepLearning/Src/DeepLearning.cpp
+++ DeepLearning/Src/DeepLearning.cpp
@@ -13,15 +13,34 @@ extern "C"
 #include "lauxlib.h"
 }
 
+int Add(lua_State* L)
+{
+	int n = lua_gettop(L);
+
+	double sum = 0;
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		sum += lua_tonumber(L, i);
+	}
+
+	lua_pushnumber(L, sum);
+	return 1;
+}
+
 int main()
 {
 	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
 	luaL_dofile(L, "Scripts/hello.lua");
 
+	lua_register(L, "cAdd", Add);
+
 	lua_getglobal(L, "fib");
 	lua_pushnumber(L, 13);
-	lua_call(L, 1, 1);
+	lua_pushnumber(L, 17);
+	lua_call(L, 2, 1);
 	int result = (int)lua_tointeger(L, -1);
 	lua_pop(L, 1);
 

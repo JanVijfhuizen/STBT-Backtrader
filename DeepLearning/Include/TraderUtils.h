ee1db14483eecebe0e7cf1989ca4be1a0caa924e
Jannie <jan.vijfhuizen@outlook.com>
2025-04-11 11:15:43 +0200

    [Add] Trading Utils.

--- DeepLearning/Include/TraderUtils.h
+++ DeepLearning/Include/TraderUtils.h
@@ -6,5 +6,7 @@ namespace jv
 	{
 		[[nodiscard]] static Array<float> CreateMA(Arena& arena, 
 			uint32_t start, uint32_t end, uint32_t n, float* data);
+		[[nodiscard]] static float GetCorrolation(float* a, float* b, uint32_t n);
+		[[nodiscard]] static float GetVariance(float* a, uint32_t n);
 	};
 }
--- DeepLearning/Src/TraderUtils.cpp
+++ DeepLearning/Src/TraderUtils.cpp
@@ -4,7 +4,8 @@
 
 namespace jv
 {
-    Array<float> TraderUtils::CreateMA(Arena& arena, uint32_t start, uint32_t end, uint32_t n, float* data)
+    Array<float> TraderUtils::CreateMA(Arena& arena, const uint32_t start, 
+        const uint32_t end, const uint32_t n, float* data)
     {
         auto maN = CreateArray<float>(arena, start - end);
 
@@ -18,6 +19,37 @@ namespace jv
         }
 
         return maN;
+    }
+
+    float TraderUtils::GetCorrolation(float* a, float* b, const uint32_t n)
+    {
+        float aSum = 0, bSum = 0, abSum = 0;
+        float aSqrtSum = 0, bSqrtSum = 0;
+        for (uint32_t i = 0; i < n; i++)
+        {
+            aSum += a[i];
+            bSum += b[i];
+            abSum += a[i] * b[i];
+            aSqrtSum += a[i] * a[i];
+            bSqrtSum += b[i] * b[i];
+        }
+
+        float corr = (abSum * n - aSum * bSum) / sqrt((aSqrtSum * n - aSum * aSum) * (bSqrtSum * n - bSum * bSum));
+        return corr;
+    }
+
+    float TraderUtils::GetVariance(float* a, const uint32_t n)
+    {
+        float avr = 0;
+        for (uint32_t i = 0; i < n; i++)
+            avr += a[i];
+        avr /= n;
+
+        float variance = 0;
+        for (uint32_t i = 0; i < n; i++)
+            variance += pow(a[i] - avr, 2);
+        variance /= n;
+        return variance;
     }
 }
 

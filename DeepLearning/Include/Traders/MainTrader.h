b3368081c99a5b14bd7dadd72c4668e0c0eac146
Jannie <jan.vijfhuizen@outlook.com>
2025-04-29 11:36:20 +0200

    [Add] Basic MA trader.

--- DeepLearning/Include/Traders/MainTrader.h
+++ DeepLearning/Include/Traders/MainTrader.h
@@ -6,8 +6,20 @@ namespace jv
 	{
 		Arena* arena;
 		Arena* tempArena;
-
 		uint64_t runScope;
+
+		uint32_t mas1Len = 30;
+		uint32_t mas2Len = 100;
+
+		uint32_t start, end;
+
+		// Moving Averages.
+		float** mas1;
+		float** mas2;
+
+		// Buy/Sell Thresholds.
+		float* threshPos;
+		float* threshNeg;
 
 		[[nodiscard]] static MainTrader Create(Arena& arena, Arena& tempArena);
 		[[nodiscard]] jv::bt::STBTBot GetBot();
--- DeepLearning/Src/MainTrader.cpp
+++ DeepLearning/Src/MainTrader.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "Traders/MainTrader.h"
+#include "TraderUtils.h"
+#include "Jlib/VectorUtils.h"
 
 namespace jv
 {
@@ -10,6 +12,24 @@ namespace jv
 	{
 		auto mt = reinterpret_cast<MainTrader*>(userPtr);
 		mt->runScope = mt->arena->CreateScope();
+
+		mt->start = start;
+		mt->end = end;
+
+		const uint32_t count = scope.GetTimeSeriesCount();
+		mt->mas1 = mt->arena->New<float*>(count);
+		mt->mas2 = mt->arena->New<float*>(count);
+
+		for (uint32_t i = 0; i < count; i++)
+		{
+			auto close = scope.GetTimeSeries(i).close;
+			mt->mas1[i] = TraderUtils::CreateMA(*mt->arena, start, end, mt->mas1Len, close);
+			mt->mas2[i] = TraderUtils::CreateMA(*mt->arena, start, end, mt->mas2Len, close);
+		}
+
+		mt->threshPos = mt->arena->New<float>(count);
+		mt->threshNeg = mt->arena->New<float>(count);
+
 		return true;
 	}
 
@@ -17,6 +37,38 @@ namespace jv
 		uint32_t current, void* userPtr, jv::Queue<const char*>& output)
 	{
 		auto mt = reinterpret_cast<MainTrader*>(userPtr);
+		auto tempScope = mt->tempArena->CreateScope();
+
+		const uint32_t count = scope.GetTimeSeriesCount();
+		auto buys = CreateVector<uint32_t>(*mt->tempArena, count);
+		auto sells = CreateVector<uint32_t>(*mt->tempArena, count);
+
+		const uint32_t index = mt->start - current;
+
+		for (uint32_t i = 0; i < count; i++)
+		{
+			const float diff = mt->mas1[i][current] - mt->mas2[i][current];
+			if (diff > mt->threshPos[i])
+				buys.Add() = i;
+			else if (diff < -mt->threshNeg[i])
+				sells.Add() = i;
+		}
+
+		// Buy with an even distribution.
+		const auto liq = scope.GetLiquidity();
+		float stackPrice = 0;
+
+		for (auto& buy : buys)
+			stackPrice += scope.GetTimeSeries(buy).close[current];
+
+		const uint32_t stackAmount = Max<float>(liq / stackPrice, 1);
+		for (auto& buy : buys)
+			trades[buy].change = stackAmount;
+
+		for (auto& sell : sells)
+			trades[sell].change = -1e5;
+
+		mt->tempArena->DestroyScope(tempScope);
 		return true;
 	}
 
